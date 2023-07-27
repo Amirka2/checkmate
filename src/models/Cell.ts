c1b87c24d5483bcf918b84737cbedd769b94b796
amir_mac <far20022002@mail.ru>
2023-07-28 03:16:35 +0500

    added models

---
+++ src/models/Cell.ts
@@ -0,0 +1,18 @@
+import {Colors} from "./Colors";
+import {Figure} from "./figures/Figure";
+
+export class Cell {
+    id: number;
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+
+    constructor(x: number, y: number, color: Colors, figure: Figure | null) {
+        this.id = Math.random();
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+    }
+}
