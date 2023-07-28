e3dc2283362b67849d283053a27844c44d761f4a
amir_mac <far20022002@mail.ru>
2023-07-28 17:50:59 +0500

    added base class for figure

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,19 @@
-export interface Figure {
+import {Colors} from "../Colors";
+import logo from "../../../public/figures-logos/black-knight.png";
+import {Cell} from "../Cell";
 
+export enum FigureNames {
+    PAWN = 'pawn',
+    KING = 'king',
+    QUEEN = 'queen',
+    ROOK = 'rook',
+    KNIGHT = 'knight',
+    BISHOP = 'bishop',
+}
+
+export interface Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    name: FigureNames;
+    cell: Cell;
 }
