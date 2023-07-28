6dc43d24cc827cfae2c72bb9821789f0a3495c53
amir_mac <far20022002@mail.ru>
2023-07-28 18:47:23 +0500

    moved figures on a board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -4,10 +4,12 @@ import {Cell} from "../models/Cell";
 interface cellProps {
     cell: Cell
 }
-const CellComponent: FC<cellProps> = (props: cellProps) => {
+const CellComponent: FC<cellProps> = ({cell}) => {
     return (
-        <div className={['cell', props.cell.color].join(' ')}>
-            
+        <div className={['cell', cell.color].join(' ')}>
+            {cell.figure?.logo && <img src={cell.figure.logo}
+                                       alt='logo' className='logo'/>
+            }
         </div>
     );
 };
