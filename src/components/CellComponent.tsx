cda200c2e87d10d081743722059136290fe88268
amir_mac <far20022002@mail.ru>
2023-07-29 20:22:47 +0500

    added isAvailable highlight

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -5,15 +5,20 @@ interface cellProps {
     cell: Cell,
     selected: boolean,
     clickOnCell: (cell: Cell) => void,
+    isAvailable: boolean,
 }
-const CellComponent: FC<cellProps> = ({cell, selected, clickOnCell}) => {
-    const classNames = ['cell', cell.color, selected ? 'selected' : ''].join(' ');
+const CellComponent: FC<cellProps> = ({cell, selected, clickOnCell, isAvailable}) => {
+    const classNames = ['cell',
+        cell.color,
+        selected ? 'selected' : ''
+    ].join(' ');
 
     return (
         <div className={classNames} onClick={() => clickOnCell(cell)}>
             {cell.figure?.logo && <img src={cell.figure.logo}
                                        alt='logo' className='logo'/>
             }
+            {cell.figure === null && isAvailable && <div className='available'/>}
         </div>
     );
 };
