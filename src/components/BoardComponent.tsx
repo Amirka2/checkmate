3f72f020a9425dc8c900916a6774841663226c10
amir_mac <far20022002@mail.ru>
2023-07-29 19:04:59 +0500

    added cell selecting on click

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,17 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Board} from "../models/Board";
 import CellComponent from "./CellComponent";
+import {Cell} from "../models/Cell";
 
 interface boardProps {
     board: Board,
     setBoard: (board: Board) => void,
 }
 const BoardComponent: FC<boardProps> = (props: boardProps) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+    function clickOnCell(cell: Cell): void {
+        setSelectedCell(cell);
+    }
+    function isSelected(cell: Cell): boolean {
+        return cell.x === selectedCell?.x && cell.y === selectedCell?.y;
+    }
     return (
         <div className='board'>
             {props.board.cells.map((row, index) => {
                 return (<div className='row'>
-                            {row.map(cell => <CellComponent key={cell.id} cell={cell}/>)}
+                            {row.map(cell => <CellComponent
+                                key={cell.id} cell={cell} selected={isSelected(cell)}
+                                clickOnCell={clickOnCell}/>)}
                         </div>)
             })
             }
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,11 +2,15 @@ import React, {FC} from 'react';
 import {Cell} from "../models/Cell";
 
 interface cellProps {
-    cell: Cell
+    cell: Cell,
+    selected: boolean,
+    clickOnCell: (cell: Cell) => void,
 }
-const CellComponent: FC<cellProps> = ({cell}) => {
+const CellComponent: FC<cellProps> = ({cell, selected, clickOnCell}) => {
+    const classNames = ['cell', cell.color, selected ? 'selected' : ''].join(' ');
+
     return (
-        <div className={['cell', cell.color].join(' ')}>
+        <div className={classNames} onClick={() => clickOnCell(cell)}>
             {cell.figure?.logo && <img src={cell.figure.logo}
                                        alt='logo' className='logo'/>
             }
